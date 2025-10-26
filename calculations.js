9f9c66551ac166049c69b78348a317fa5fdb1ac0
Olga <alaninmg@gmail.com>
2025-10-26 23:05:33 +0200

    H-w-2.2.4: increment and decrement

--- calculations.js
+++ calculations.js
@@ -19,7 +19,18 @@ function getRestAfterDivision(x, y) {
   return x;
 }
 
-console.log(increaseBalance(3000, 700));
-console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2));
-console.log(getRestAfterDivision(7000, 3));
+function incrementBalance(x){
+  return ++x;
+}
+
+function decrementBalance(x) {
+  return --x;
+}
+
+// console.log(increaseBalance(3000, 700));
+// console.log(decreaseBalance(3000, 700));
+// console.log(divideBalanceByAccounts(3000, 2));
+// console.log(getRestAfterDivision(7000, 3));
+
+console.log(incrementBalance(10));
+console.log(decrementBalance(10));
